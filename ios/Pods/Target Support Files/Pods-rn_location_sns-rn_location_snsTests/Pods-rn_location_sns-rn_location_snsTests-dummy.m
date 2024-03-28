c7d160dd3fc30bc66b7d43ab8324e9fbb5ea61aa
seehun <hwangbird99@gmail.com>
2024-03-28 10:37:02 +0900

    initial setting

---
+++ ios/Pods/Target Support Files/Pods-rn_location_sns-rn_location_snsTests/Pods-rn_location_sns-rn_location_snsTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_rn_location_sns_rn_location_snsTests : NSObject
+@end
+@implementation PodsDummy_Pods_rn_location_sns_rn_location_snsTests
+@end
